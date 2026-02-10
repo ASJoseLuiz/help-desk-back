44365cbf7f6462a6f7775447e38b3b3e735f52f0
José Luiz <joseluix4824@gmail.com>
2026-02-09 21:48:23 -0300

    feat: add CreateUserService

---
+++ src/modules/services/CreateUserService.ts
@@ -0,0 +1,28 @@
+import CreateUserDTO from "../dto/CreateUserDTO";
+import GetUserDTO from "../dto/GetUserDTO";
+import IUserRepository from "../repository/IUserRepository";
+import { inject, injectable } from "tsyringe";
+
+@injectable()
+export default class CreateUserService {
+  constructor(
+    @inject("UserRepository") private userRepository: IUserRepository,
+  ) {}
+
+  async execute(data: CreateUserDTO): Promise<GetUserDTO> {
+    const userExists = await this.userRepository.findByEmail(data.email);
+
+    if (userExists) {
+      throw new Error("User already exists");
+    }
+
+    const user = await this.userRepository.create(data);
+
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      createdAt: user.createdAt.toISOString(),
+    };
+  }
+}
