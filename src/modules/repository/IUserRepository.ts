896998a1f73ba8eef47c46ce68c8cf88d3b13ef8
José Luiz <joseluix4824@gmail.com>
2026-02-09 21:47:53 -0300

    feat: add user repository interface

---
+++ src/modules/repository/IUserRepository.ts
@@ -0,0 +1,12 @@
+import { User } from "../../generated/prisma/client";
+import CreateUserDTO from "../dto/CreateUserDTO";
+import UpdateUserDTO from "../dto/UpdateUserDTO";
+
+export default interface IUserRepository {
+  create(data: CreateUserDTO): Promise<User>;
+  findById(id: string): Promise<User | null>;
+  findByEmail(email: string): Promise<User | null>;
+  findAll(): Promise<User[]>;
+  update(id: string, data: UpdateUserDTO): Promise<User>;
+  delete(id: string): Promise<void>;
+}
