6c7f73135fb22019721941d9ca827fffcf23043c
José Luiz <joseluix4824@gmail.com>
2026-02-21 15:35:55 -0300

    feat: app use error handler middleware

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import "reflect-metadata";
 import express from "express";
 import "./shared/config/container";
 import routes from "./shared/router";
+import { errorHandler } from "./shared/middlewares/ErrorHandler";
 
 const app = express();
 app.use(express.json());
+app.use(errorHandler);
 
 app.use(routes);
 
