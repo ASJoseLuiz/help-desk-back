6a6502d4a6f6b75be2f29abc5b5ead69f5d11f9c
José Luiz <joseluix4824@gmail.com>
2026-03-08 16:36:43 -0300

    feat: add container

--- src/shared/config/container/index.ts
+++ src/shared/config/container/index.ts
@@ -3,6 +3,9 @@ import { container } from "tsyringe";
 import { prismaClient } from "../prisma/client";
 import IUserRepository from "../../../modules/users/repository/IUserRepository";
 import UserRepository from "../../../modules/users/repository/UserRepository";
+import TicketRepository from "../../../modules/tickets/repository/Repository";
+import ITicketRepository from "../../../modules/tickets/repository/ITicketsRepository";
 
 container.registerInstance("PrismaClient", prismaClient);
 container.registerSingleton<IUserRepository>("UserRepository", UserRepository);
+container.registerSingleton<ITicketRepository>("TicketRepository", TicketRepository);
