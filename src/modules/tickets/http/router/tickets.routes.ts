b9a5b3fce84087066898c2252efe7a07034a6242
José Luiz <joseluix4824@gmail.com>
2026-03-08 16:44:23 -0300

    feat: index route and apply middleware

--- src/modules/tickets/http/router/tickets.routes.ts
+++ src/modules/tickets/http/router/tickets.routes.ts
@@ -3,9 +3,11 @@ import TicketController from "../controller/TicketController";
 import { validateBody } from "../../../../shared/middlewares/ZodValidation";
 import { createTicketSchema } from "../../dto/CreateTicketDTO";
 import { updateTicketSchema } from "../../dto/UpdateTicketDTO";
+import { authMiddleware } from "../../../../shared/middlewares/AuthMiddleware";
 
 const ticketRouter = Router();
 const ticketController = new TicketController()
+ticketRouter.use(authMiddleware);
 
 ticketRouter.post("/", validateBody(createTicketSchema), ticketController.create);
 ticketRouter.get("/", ticketController.getTickets);
