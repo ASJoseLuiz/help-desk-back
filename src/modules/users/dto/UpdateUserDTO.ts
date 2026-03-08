bc0452c1f32f75145dd2c560ee22a4866705139e
José Luiz <joseluix4824@gmail.com>
2026-03-08 15:16:09 -0300

    fix: adjust dto's

--- src/modules/users/dto/UpdateUserDTO.ts
+++ src/modules/users/dto/UpdateUserDTO.ts
@@ -1,7 +1,11 @@
 import z from "zod";
+import { Role } from "../../../generated/prisma/enums";
 
 export const updateUserSchema = z.object({
   name: z.string().min(1).max(30, "Nome pode ter no máximo 30 caracteres").optional(),
   email: z.string().email("Email inválido").optional(),
   password: z.string().min(6, "Senha deve ter no mínimo 6 caracteres").optional(),
+  role: z.enum(Role).optional()
 });
+
+export type UpdateUserDTO = z.infer<typeof updateUserSchema>;
