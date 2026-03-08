2aea78c3ad3946fea9e89fd2ade0d76c70efee59
José Luiz <joseluix4824@gmail.com>
2026-03-08 16:45:33 -0300

    fix: import

--- src/shared/config/container/index.ts
+++ src/shared/config/container/index.ts
@@ -3,7 +3,7 @@ import { container } from "tsyringe";
 import { prismaClient } from "../prisma/client";
 import IUserRepository from "../../../modules/users/repository/IUserRepository";
 import UserRepository from "../../../modules/users/repository/UserRepository";
-import TicketRepository from "../../../modules/tickets/repository/Repository";
+import TicketRepository from "../../../modules/tickets/repository/TicketRepository";
 import ITicketRepository from "../../../modules/tickets/repository/ITicketsRepository";
 
 container.registerInstance("PrismaClient", prismaClient);
